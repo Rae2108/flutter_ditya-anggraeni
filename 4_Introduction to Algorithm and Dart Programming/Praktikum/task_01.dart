0b5c42559223e328c2dece7db8d1bc09ff8cf521
Rae2108 <95254604+Rae2108@users.noreply.github.com>
2022-02-24 10:10:52 +0700

    Task Section 4

---
+++ 4_Introduction to Algorithm and Dart Programming/Praktikum/task_01.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  print('Membuat rumus lingkaran');
+  final phie = 3.14;
+  int er = 11;
+  double luaslingkaran = phie*(er*er);
+  print ('Hasil dari lingkaran dengan jari-jari $er adalah $luaslingkaran');
+}
---
+++ 4_Introduction to Algorithm and Dart Programming/Praktikum/task_02.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  String nama = 'Ditya Anggraeni';
+  String kelas = 'Flutter Kelompok C';
+  String jk = 'P';
+
+  print ('Nama saya $nama jenis kelamin saya $jk dari kelas $kelas');
+}
