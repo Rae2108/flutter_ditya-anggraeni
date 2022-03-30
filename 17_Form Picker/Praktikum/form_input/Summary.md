575bd9c2f6d63b84dfe50cec0d6e92fe18e26fc2
Rae2108 <95254604+Rae2108@users.noreply.github.com>
2022-03-30 16:59:41 +0700

    Update

---
+++ 17_Form Picker/Praktikum/form_input/Summary.md
@@ -0,0 +1,43 @@
+# 17_Form_Input
+
+Nama : Ditya Anggraeni
+
+Program : Become a Flutter Master, From Zero to Hero
+
+Repo : https://github.com/Rae2108/flutter_ditya-anggraeni
+
+## Tuliskan 3 poin yang dipelajari dari materi tersebut. Resume / ringkasan materi dapat disubmit melalui Github
+
+### Jawab : 
+
+1. Data Picker merupakan widget dimana user bisa memasukkan tanggal. Contoh seperti tanggal kelahiran, waktu pemesanan, waktu reserve di restoran.
+- Cara membuat data picker :
+• Menggunakan fungsi bawaan flutter ShowDatePicker
+• Fungsi ShowDataPicker memiliki tipe data future
+• Menampilkan dialog materialdesign date picker
+• Menambahkan packages intl di Pubspec.yaml
+• Mempersiapkan variabel
+• Membangun UI
+• Menambahkan ShowDatePicker di dalam onPressed
+• Memanggil fungsi state di dalam onPressed 
+
+2. Color Picker merupakan widget dimana user dapat memilih color, penggunaan color picker bisa digunakan untuk berbagai macam kondisi.
+- Cara membuat file picker
+•  Menggunakan packages flutter_colorpicker
+• Menambahkan packages flutter_colorpicker di Pubspec.yaml
+• Mempersiapkan variabel
+• Membangun UI
+• Menambahkan fungsi ShowDialog di dalam onPresses dengan return widget AlertDialog
+• Import packages flutter_colorpicker dalam file dart
+• Membuat kode untuk penggunaan pckages flutter_color picker 
+
+3. File picker merupakan kemampuan widget untuk mengakses storage dapat memilih dan membuka file
+- Cara membuat File Picker
+•  Menggunakan packages file_picker dan open_file.
+• Menambahkan packages file_picker dan open_file di Pubspec.yaml
+• Import packages file_picker dan open^file dalam file dart
+• Membangun UI
+• Membuat fungsi untuk mengeksplore dan memilih files dari storage
+• Memanggil fungsi_picfile di dalam onPressed
+• Membuat fungsi untuj membuka files yang telah dipilih
+• Mengambil file dari object result dan memanggil fungsi_open di dalam fungsi_picfile
--- 17_Form Picker/Praktikum/form_input/lib/main.dart
+++ 17_Form Picker/Praktikum/form_input/lib/main.dart
@@ -1,7 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,26 +11,131 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Create Post',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Create Post'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime _dueDate = DateTime.now();
+  final currentDate = DateTime.now();
+  Color _currentColor = Colors.brown;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Form Input'),
-        centerTitle: true,
+        title: Text(widget.title),
       ),
       body: Container(
         padding: const EdgeInsets.all(16),
         child: ListView(
           children: [
-            const Text('Pick Files'),
-            const SizedBox(height: 8),
-            Center(
-              child: ElevatedButton(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Pick Files'),
+                ElevatedButton(
                 child: const Text('Pick files'),
                 onPressed: (){
                   _pickFile();
                 },
+              ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Tanggal Posting'),
+                TextButton(
+                  child: const Text('Select'),
+                  onPressed: () async{
+                    final selectDate = await showDatePicker(
+                      context: context,
+                      initialDate: currentDate,
+                      firstDate: DateTime(1990),
+                      lastDate: DateTime(currentDate.year + 5),
+                    );
+
+                    setState((){
+                      if(selectDate != null){
+                        _dueDate = selectDate;
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            const Text('Pilih Warna'),
+            // Container(
+            //   height: 100,
+            //   width: double.infinity,
+            //   color: _currentColor,
+            // ),
+            const SizedBox(height: 8),
+            Center(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(_currentColor),
+                ),
+                child: const Text('Pick Color'),
+                onPressed: (){
+                  showDialog(
+                    context: context, 
+                    builder: (context){
+                      return AlertDialog(
+                        title: const Text('Pick Color'),
+                        content: BlockPicker(
+                          pickerColor: _currentColor,
+                          onColorChanged: (color){
+                            setState(() {
+                              _currentColor = color;
+                            });
+                          },
+                        ),
+                      );
+                    }
+                  );
+                },
+              ),
+            ),
+            const Text('Caption :'),
+            const SizedBox(height: 8),
+            Column(
+              children: <Widget>[
+                Card(
+                  color: Colors.white,
+                  child: TextField(
+                    maxLines: 8,
+                    // decoration: InputDecoration.collapsed(hintText: 'Enter'),
+                  )
+                )
+              ],
+            ),
+            Center(
+              child: ElevatedButton(
+                child: const Text('Submit'),
+                onPressed: (){
+                  
+                },          
               ),
             ),
           ],
@@ -41,14 +147,12 @@ class MyApp extends StatelessWidget {
 
 void _pickFile() async{
   FilePickerResult? result = await FilePicker.platform.pickFiles();
-  if(result != null){
-  }else{}
+  if(result == null) return;
 
-  //
-  // final file = result.files.first;
-  // _openFile(file);
+  final file = result.files.first;
+  _openFile(file);
 }
 
-// void _openFile(PlatformFile file){
-//   OpenFile.open(file.path);
-// }
+void _openFile(PlatformFile file){
+  OpenFile.open(file.path as String);
+}
